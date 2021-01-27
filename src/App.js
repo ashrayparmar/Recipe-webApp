1ed5293189c5688692847497ca2e7440e2ed7278
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-27 20:04:29 +0530

    feat: new component for fetching recipe's

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, {Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 
 const App = () => {
 
   const APP_ID = '74b1c963';
   const API_KEY = '641ce4efe4ceb5ffd8935e653e5debfa';
 
-  const Req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(()=> {
+    getRecipes();
+  });
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  }
 
   return (
     <div className="App">
-      
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => {
+        <Recipe />
+      })}
     </div>
   );
 }
