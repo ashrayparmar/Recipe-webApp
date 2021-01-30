7716f1185029b6efe7fd60a5b01fcd7af885fdfd
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-01-30 14:29:39 +0530

    style: display border done

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './recipe.module.css'
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
-            <h2>{calories}</h2>
+            <h2>Calories: {calories}</h2>
             <img src={image} alt="img"></img>
             <ol>{ingredients.map(ingredient => (
                 <li>{ingredient.text}</li>
